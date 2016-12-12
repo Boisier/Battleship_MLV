985ecd0198bb20aec599f907d396d3b20fa2fc64
padawan <padawan@debianUPEM-2016>
2016-12-12 21:08:27 +0100

    MLV Test

--- main.c
+++ main.c
@@ -10,11 +10,31 @@ int main(int argc, char *argv[])
 {
 	//Game has been lauched;
 
-	GameObj gameObj = initGame();
-
-	gameObj.currTurn = 0;
-	printf("%d\n", gameObj.currTurn);
-	printf("success\n");
+	//
+	// Créé et affiche la fenêtre
+	//
+	MLV_create_window( "beginner - 1 - hello world", "hello world", 640, 480 );
+	//
+	// Affiche "Bonjour !" a l'écran 
+	//
+	MLV_draw_text(
+			10, 120, 
+			"Bonjour !",
+			MLV_COLOR_MAGENTA
+	);
+	//
+	// Met a jour l'affichage.
+	//
+	MLV_actualise_window();
+	//
+	// Attend 10 secondes avant la fin du programme.
+	//
+	MLV_wait_seconds( 10 );
+	//
+	// Ferme la fenêtre
+	//
+	MLV_free_window();
+	return 0;
 
 	return 0;
  }
