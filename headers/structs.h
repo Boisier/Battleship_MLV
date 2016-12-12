e54263c1071acc7373be7413c3e7d0695579e89d
padawan <padawan@debianUPEM-2016>
2016-12-12 15:48:43 +0100

    header.h update &

--- headers/structs.h
+++ headers/structs.h
@@ -1,14 +1,33 @@
 #pragma once
 
+#define bool int
+#define true 1
+#define false 0
+
 typedef struct Player
 {
 
 } Player;
 
+typedef struct Cell
+{
+    char type;              //Type of the cell (e -> empty, s -> ship);
+    bool hit;               //Has the cell been hit or not? (true or false)
+}Cell;
+
+typedef struct Grid
+{
+    int size;               //Size of the grid
+    Cell *cells;            //Celles of the grid
+    Ship *ships             //Ships on the grid
+} Grid;
+
 typedef struct Ship
 {
-    int size;
-    char direction;
-    int posX;
-    int posY;
+    int size;               //Size of the ship (1, 2, 3, 4 or 5)
+    char direction;         //vertical or horizontal (v or h)
+    int posX;               //X position of the ship on the grid
+    int posY;               //Y position of the ship on the grid
+    bool hits[5];           //Details which cell of the ship has been hit;
+
 } Ship;
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <MLV/MLV_all.h>
+#include "headers/structs.h"
 
 int main(int argc, char *argv[])
 {
